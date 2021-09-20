552d7ac38439fb9bf667302aeb3b4b8702c6b8e8
Md Ali <mdali251@yahoo.com>
2021-09-20 07:56:06 -0500

    broken mpi

--- Homework/Programming-Assignments/PA-1/exercise1.c
+++ Homework/Programming-Assignments/PA-1/exercise1.c
@@ -15,7 +15,7 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
 
 // comparison? 
 static int compare_int(const void *a, const void *b)
@@ -24,11 +24,33 @@ static int compare_int(const void *a, const void *b)
 }
 
 // unfinised looking thorugh code at the momnet
+// is merge sort the way to go?
+// how to make this represetnated for multiple threads????  
 static void merge(int *a, int numel_a, int *b, int numel_b)
 {
 	int *sorted = (int *) malloc((numel_a + numel_b) * sizeof *a);
 	int i, a_i = 0, b_i = 0; 
 	
 	for (i = 0; i < (numel_a + numel_b); i++) {
-		if(a_i < numel_a && b_i < numel_b) { 
+		if(a_i < numel_a && b_i < numel_b) {
+			if(a[a_i] < b[b_i]) { 
+			sorted[i] = a[a_i];
+			a_i++;		
+		}
+		else {
+			sorted[i] = b[b_i];
+			b_i++;
+		}
+	else { 
+		if(a_i < numel_a) {
+		sorted[i] = a[a_i];
+		a_i++;}	
+ else {
+	sorted[i] = b[b_i];
+	b_i++;
+	}
 }
+}
+}}
+
+
