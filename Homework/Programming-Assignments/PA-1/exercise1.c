691d75dc22f911961ef1a2fcd61e74e173397f5e
xXxSpicyBoiiixXx <mali54@hawk.iit.edu>
2021-09-26 15:00:46 -0500

    modifying example code for problem 1

--- Homework/Programming-Assignments/PA-1/exercise1.c
+++ Homework/Programming-Assignments/PA-1/exercise1.c
@@ -17,6 +17,15 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <time.h>
+
+#define NUM_ELEMENTS 10
+
+static void print_array(int * arr, size_t len) { 
+	for(int i = 0; i < len; i++)
+		printf("%d", arr[i]); 
+	printf("\n");	
+}
 
 // comparison? 
 static int compare_int(const void *a, const void *b)
@@ -52,12 +61,68 @@ static void merge(int *a, int numel_a, int *b, int numel_b)
 }
 }
 }
-<<<<<<< HEAD
-}
-
-=======
->>>>>>> d4d3f7fb05ca0d2f3b8929fb88ca380401eee301
 
 memcpy(a, sorted, (numel_a + numel_b) * sizeof *sorted);
 free(sorted);
+}
+
+int main(int argc, char **argv) 
+{
+	int rank, size, data[NUM_ELEMENTS]; 
+	MPI_Init(&argc, &argv); 
+	
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
+	MPI_Comm_size(MPI_COMM_WORLD, &size); 
+
+	if(size != 2) { 
+		printf("Please run with exactly 2 rankss\n"); 
+		MPI_Finalize(); 
+		exit(0); 
+	} 
+
+	int first_half = floor((double) NUM_ELEMENTS / 2); 
+	int second_half = NUM_ELEMENTS - first_half; 
+
+	srand(0); 
+
+	if(rank == 0) {
+		int i; 
+		printf("Unsorted:\t");
+		
+		print_array(data, NUM_ELEMENTS); 
+	
+	// sends data to the other rank
+	MPI_Send(&data[first_half], second_half, MPI_INT, 1, 0, MPI_COMM_WORLD); 
+	
+	// sorts the first half
+	qsort(data, first_half, sizeof(int), compare_int); 
+
+	// receive sorted second half of the data
+	MPI_Recv(&data[first_half], second_half, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
+
+	// merging the two sorted arrays
+	merge(data, first_half, &data[first_half], second_half);
+
+	printf("Sorted:\t\t"); 
+	
+	for(i = 0; i < NUM_ELEMENTS; i++)
+		printf("%d ", data[i]);
+	printf("\n"); 
+
+	} else if (rank == 1) {
+		
+	// receives half of the data 
+	MPI_Recv(data, second_half, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+	// sort the received data 
+	qsort(data, second_half, sizeof(int), compare_int); 
+	
+	// send back sorted data 
+	MPI_Send(data, second_half, MPI_INT, 0, 0, MPI_COMM_WORLD);	
+ 	}
+
+MPI_Finalize();
+
+return 0;
+
 }
