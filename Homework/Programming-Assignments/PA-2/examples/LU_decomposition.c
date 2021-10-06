98a95cd05ad5638dc48d1a3ac3f1812610020c99
Md Ali <mali54@hawk.iit.edu>
2021-10-06 15:25:36 -0500

    examples for PA2

---
+++ Homework/Programming-Assignments/PA-2/examples/LU_decomposition.c
@@ -0,0 +1,24 @@
+/*
+ * LU Methodolgy
+ * Assumpiton: All order principal minors are non-zero
+ */ 
+
+
+// https://livedu.in/lu-decomposition-method-algorithm-implementation-in-c-with-solved-examples/
+// Go through the above webstie later,.
+#include <stdio.h> 
+
+void main() { 
+	int k, n; 
+
+	float a[10][10], l[10],[10], u[10][10], z[10], x[10], b[10]; 
+
+	printf("\nEnter the dimenstions of the matrix: "); 
+	scanf("%d", &n); 
+
+	printf("Enter row elements: ");
+
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j 
+	}
+}
