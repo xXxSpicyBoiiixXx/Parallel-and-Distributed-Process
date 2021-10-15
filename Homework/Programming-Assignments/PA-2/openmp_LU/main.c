58aa1cebed901b94ee49e9cd16a363c23fee8323
Md Ali <mali54@hawk.iit.edu>
2021-10-14 21:54:12 -0500

    Adding file

--- Homework/Programming-Assignments/PA-2/openmp_LU/main.c
+++ Homework/Programming-Assignments/PA-2/openmp_LU/main.c
@@ -1,3 +1,7 @@
+/*
+ * 
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
@@ -129,15 +133,15 @@ void initializeLoop2(double **A,long n){
 }
 
 
-double **getMatrix(long size,long version)
+double **getMatrix(long size,long variation)
 {
 	double **m=make2dmatrix(size);
 	switch(version){
 	case 1:
-		initializeVersion1(m,size);
+		initializeLoop1(m,size);
 		break;
 	case 2:
-		initializeVersion2(m,size);
+		initializeLoop2(m,size);
 		break;
 	default:
 		printf("Please eneter either 1 or 2 for the loop variation...");
@@ -171,7 +175,7 @@ int main(int argc, char *argv[]){
 		scanf("%lu",&matrix_size);
 
 		printf("Enter the variation number = ");
-		scanf("%lu",&version);
+		scanf("%lu",&variation);
         
         printf("Enter the number of threads");
         scanf("%lu",&num_threads);
@@ -223,10 +227,8 @@ double **make2dmatrix(long n)
 	return m;
 }
 
-// only works for dynamic arrays:
 void printMatrix(double **A, long n)
 {
-	printf("\n *************** MATRIX ****************\n\n");
 	long i, j;
 	for (i=0;i<n;i++)
 	{
@@ -236,64 +238,11 @@ void printMatrix(double **A, long n)
 	}
 }
 
-<<<<<<< HEAD
-void free2dmatrix(double ** M, long n)
+void free2dmatrix(double ** A, long n)
 {
 	long i;
-	if (!M) return;
+	if (!A) return;
 	for(i=0;i<n;i++)
-		free(M[i]);
-	free(M);
-=======
-
-
-int main(int argc, char *argv[]) { 
-	
-	clock_t start, end;
-	double time_spent; 
-	double **matrix;
-
-	if(argc != 4) { 
-		printf("Enter the size of the matrix (N x N) where N = "); 
-		scanf("%lu", &matrix_size); 
-
-		printf("Enter the version number = ");
-		scanf("%lu", &variation); 
-	} else { 
-		matrix_size = atol(argv[1]); 
-		variation = atol(argv[2]); 
-	}
-
-	//long num_threads = atol(argv[3]); 
-	/*
-	if(num_threads < 1) { 
-		num_threads = 5;
-	}*/
-
-	omp_set_num_threads(2); 
-
-	matrix = getMatrix(matrix_size, variation); 
-
-	printMatrix(matrix, matrix_size);
-
-	start = clock(); 
-	
-	decomposeOpenMP(matrix, matrix_size); 
-
-	end = clock(); 
-
-	time_spent = ((double)(end - start)) / CLOCKS_PER_SEC; 
-
-	printMatrix(matrix, matrix_size);
-	printf("\n"); 
-	printf("Size of Matrix :%lu \n", matrix_size);
-	printf("Loop Variation Number : %lu\n", variation);
-	printf("%s", checkSum(matrix, matrix_size, variation) == 1? "Decomposition successful... \n":"Decomposition failed...\n"); 
-	printf("computation Time: %f seconds\n", time_spent); 
-
-	free2dmatrix(matrix, matrix_size);
-
-	return 0;	
-
->>>>>>> 068b4151f4ee69cdc401643903e7f4f1aa39fc9c
+		free(A[i]);
+	free(A);
 }
