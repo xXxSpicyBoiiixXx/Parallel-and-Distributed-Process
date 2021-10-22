c59f89c2d4899df724236fd0e6de6be770015dc1
Md Ali <mali54@hawk.iit.edu>
2021-10-22 17:06:05 -0500

    Adding makefile setup and function for matrix

--- Homework/Programming-Assignments/PA-3/mpidecompose.c
+++ Homework/Programming-Assignments/PA-3/mpidecompose.c
@@ -5,9 +5,7 @@
 
 
 // Local matrix depening on MPI nodal region. 
-void decomposeMPI(double **A, long n, int argc, char *argv[]) {
-
-	
+void decomposeMPI(double **A, long n, int argc, char *argv[]) {	
 
 	MPI_Status status;  
 	
@@ -19,6 +17,36 @@ void decomposeMPI(double **A, long n, int argc, char *argv[]) {
 
 	time_start = MPI_Wtime(); 
 
+	int pid; 
+	int partner; 
+	int nprocs; 
+	int i, j, k, p, min, max; 
+	int nrows; 
+	int ierr; 
+	
+	
+	int data_tag = 9999; 
+	int root_process = 0;
+
+	double rowk[n];
+
+	nrows = n/nprocs; 
+
+	min = pid * nprocs;
+	max = min + nrows - 1; 
+	
+	// Finsh logic here when you get the time, might not be that important 
+
+	if(pid == nprocs - 1 && (n - (max + 1)) > 0) {
+		max = n - 1;
+	}
+
+	for(k = 0; k < n; k++) { 
+		if(k >= min && k <= max) { 
+			
+		}
+	}
+	
 }
 
 
